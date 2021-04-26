a92aa2e9d271fc54826cf301fe08f607d012b86d
cyashu99 <34243467+cyashu99@users.noreply.github.com>
2021-04-26 20:00:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1= Math.floor(Math.random() * 6) + 1;
+var left="images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src",left);
+
+var randomNumber2= Math.floor(Math.random() * 6) + 1;
+var right="images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src",right);
+
+if(randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML="🚩Player 1 wins";
+}
+else if(randomNumber2>randomNumber1) {
+    document.querySelector("h1").innerHTML="Player 2 wins🚩";
+}
+else {
+    document.querySelector("h1").innerHTML="Draw";
+}
